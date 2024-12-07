b34bb638c9fb992e983cbaddd2633454d9286904
Juan <juan.perez@unilibre.edu.co>
2024-12-07 13:14:27 -0500

    se añade el .env.example

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     port: process.env.MYSQL_PORT, // 3306
     user: process.env.MYSQL_USER, // root
     password: process.env.MYSQL_PASSWORD, // ""
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
 })
 
 connection.connect(function (error) {
