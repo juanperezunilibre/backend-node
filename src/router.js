e748d38af643a97af1e088f81feb0612aaf46dcc
Juan <juan.perez@unilibre.edu.co>
2024-12-11 22:06:32 -0500

    editar y elimibar

--- src/router.js
+++ src/router.js
@@ -1,31 +1,26 @@
 const express = require("express")
 const yup = require("yup")
 
-const { saludarConNombre, sumar } = require("./utils")
-
 const connection = require("./db") // importar la conexión a la base de datos
 
 const router = express.Router()
 
+const schema = yup.object().shape({
+  documento: yup
+    .string()
+    // .matches(/^[0-9]{7,10}$/, "El documento no es válido - min 7 max 10 dígitos")
+    .required("El documento es obligatorio"),
+  nombre: yup.string().required("El nombre es obligatorio"),
+  apellidos: yup.string().required("Los apellidos son obligatorios"),
+  email: yup
+    .string()
+    .email("El email es inválido")
+    .required("El email es obligatorio"),
+})
+
 router.get("/", function (request, response) {
   response.send("Hello World")
 }) // obtener datos
-
-router.get("/saludar", function (request, response) {
-  const saludo = saludarConNombre("Pepito")
-  const suma = sumar(5, 3)
-  response.send(saludo + " " + suma)
-})
-
-router.get("/usuario", function (request, response) {
-  response.json({
-    nombre: "Juan",
-    apellido: "Perez",
-    edad: 25,
-    email: "juan.perez@unilibre.edu.co",
-    telefono: "1234567890",
-  })
-})
 
 router.get("/clientes", function (request, response) {
   connection.query(
@@ -41,19 +36,6 @@ router.get("/clientes", function (request, response) {
 })
 
 router.post("/cliente", async function (request, response) {
-  const schema = yup.object().shape({
-    documento: yup
-      .string()
-      // .matches(/^[0-9]{7,10}$/, "El documento no es válido - min 7 max 10 dígitos")
-      .required("El documento es obligatorio"),
-    nombre: yup.string().required("El nombre es obligatorio"),
-    apellidos: yup.string().required("Los apellidos son obligatorios"),
-    email: yup
-      .string()
-      .email("El email es inválido")
-      .required("El email es obligatorio"),
-  })
-
   const datos = request.body
 
   try {
@@ -61,7 +43,7 @@ router.post("/cliente", async function (request, response) {
     console.log(result)
 
     const query =
-      "INSERT INTO clientes (documento, nombre, apellidos, email) VALUES (?, ?, ?, ?)"
+      "INSERT INTO clientes (documento, nombre, apellidos, email, fecha_nacimiento) VALUES (?, ?, ?, ?, ?)"
 
     connection.execute(query, Object.values(datos), function (error, result) {
       if (error) {
@@ -81,6 +63,84 @@ router.post("/cliente", async function (request, response) {
       message: error.message,
     })
   }
+})
+
+router.get("/cliente/:id", function (request, response) {
+  const { id } = request.params
+
+  const query = "SELECT * FROM clientes WHERE id = ?"
+
+  connection.query(query, [id], function (error, result) {
+    if (error) {
+      response.status(400).json({
+        message: "Error al obtener el cliente",
+        error: error,
+      })
+    } else {
+      if (result.length > 0) {
+        response.json(result[0])
+      } else {
+        response.status(404).json({
+          message: "Cliente no encontrado",
+        })
+      }
+    }
+  })
+})
+
+router.put("/cliente/:id", async function (request, response) {
+  const data = request.body
+  const { id } = request.params
+
+  //const query = "UPDATE clientes SET ? WHERE id = ?"
+  try {
+    await schema.validate(data) // validamos que el objeto cumpla con el esquema
+
+    const query =
+      "UPDATE clientes SET documento = ?, nombre = ?, apellidos = ?, email = ?, fecha_nacimiento = ? WHERE id = ?"
+  
+    connection.execute(
+      query,
+      Object.values(data).concat(id),
+      function (error, result) {
+        if (error) {
+          response.status(400).json({
+            message: "Error al actualizar el cliente",
+            error,
+          })
+        } else {
+          response.json({
+            message: "Cliente actualizado",
+            data: result,
+          })
+        }
+      },
+    )
+  } catch (error) {
+    response.status(400).json({
+      message: error.message,
+    })
+  }
+}) // cierre del put
+
+router.delete("/cliente/:id", function (request, response){
+  const {id} = request.params
+
+  const query = "DELETE FROM clientes WHERE id = ?"
+
+  connection.execute(query, [id], function(error, result){
+    if (error) {
+      response.status(400).json({
+        message: "Error al eliminar el cliente",
+        error
+      })
+    } else {
+      response.json({
+        message: "Cliente eliminado correctamente",
+        data: result
+      })
+    }
+  })
 })
 
 // desde 200 hasta 299 - OK
