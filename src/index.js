86064c34121db59b55cb708df0c802a0d1549c3c
Juan Perez <juanperez@MacBook-Pro-de-Wilson.local>
2024-12-20 19:28:49 -0500

    se añade origin vercel

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173", "https://clientes-creditos.netlify.app"]
+    origin: ["http://localhost:5173", "https://clientes-creditos.netlify.app", "https://react-ts-example-alpha.vercel.app"]
 })) // aceptar peticiones de cualquier origen
 app.use(express.json())
 app.use(router)
